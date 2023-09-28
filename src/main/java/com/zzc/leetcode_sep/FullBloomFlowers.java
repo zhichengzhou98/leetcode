b0b039bc1bd89322607ef7ab3bf1878eb9d6f9cc
zc.zhou <zzc@renren.com>
2023-09-28 16:26:20 +0800

    FullBloomFlowers

--- src/main/java/com/zzc/leetcode_sep/FullBloomFlowers.java
+++ src/main/java/com/zzc/leetcode_sep/FullBloomFlowers.java
@@ -3,6 +3,8 @@ package com.zzc.leetcode_sep;
 import com.zzc.utils.ArrayUtils;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * @author zc.zhou
@@ -17,6 +19,22 @@ public class FullBloomFlowers {
         System.out.println(Arrays.toString(fBF.fullBloomFlowers(f, p)));
     }
     public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+        // 使用treemap
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < flowers.length; i++) {
+            map.put(flowers[i][0],map.getOrDefault(flowers[i][0], 0) + 1);
+            map.put(flowers[i][1] + 1, map.getOrDefault(flowers[i][1] + 1, 0)-1);
+        }
+        int firstKey = map.firstKey();
+        AtomicInteger lastElement = new AtomicInteger(0);
+        //0 为时间， 1 为数量
+        List<int[]> list = map.entrySet().stream().map(e ->
+                new int[]{e.getKey(), lastElement.addAndGet(e.getValue())}
+        ).collect(Collectors.toList());
+
+        return Arrays.stream(people).map(i -> i < firstKey ? 0 : binarySearch(list, i)).toArray();
+    }
+    /*public int[] fullBloomFlowers(int[][] flowers, int[] people) {
         // 使用treemap
         TreeMap<Integer, Integer> map = new TreeMap<>();
         for (int i = 0; i < flowers.length; i++) {
@@ -45,14 +63,14 @@ public class FullBloomFlowers {
             }
         }
         return res1;
-    }
+    }*/
 
     public int binarySearch(List<int[]> list, int target){
         int l = 0;
         int r = list.size() - 1;
         int med = (r - l + 1 )/ 2 + l;
         while (l < r) {
-            //不超过target的最大值
+            //不超过target的最大值， 不超过 target时为true
             if (checkMedRight(list.get(med)[0], target)) {
                 l = med;
             }else {
