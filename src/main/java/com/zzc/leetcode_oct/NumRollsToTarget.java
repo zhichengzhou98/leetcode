fb4327341fe0ae72fefa797cd3c481a9d959a118
zc.zhou <zzc@renren.com>
2023-10-24 21:15:57 +0800

    NumRollsToTarget

--- src/main/java/com/zzc/leetcode_oct/NumRollsToTarget.java
+++ src/main/java/com/zzc/leetcode_oct/NumRollsToTarget.java
@@ -7,7 +7,7 @@ package com.zzc.leetcode_oct;
  */
 public class NumRollsToTarget {
     public static void main(String[] args) {
-        System.out.println(numRollsToTarget(3, 6, 3));
+        System.out.println(numRollsToTarget(30, 30, 500));
     }
     private static int MOD = (int) (Math.pow(10, 9) + 7);
 
@@ -22,10 +22,12 @@ public class NumRollsToTarget {
         for (int i = 2; i < n + 1; i++) {
             for (int j = 1; j < target + 1; j++) {
                 long a = 0L;
-                for (int m = 1; m <= i - 1; m++) {
-                    for (int l = 1; l <= j - 1; l++) {
-                        a = (a + (res[m][l] * res[i-m][j-l] % MOD)) % MOD;
-                    }
+                int minL = j - i + 1;
+                minL = Math.min(minL, k);
+                for (int l = Math.max(1, (j - (i - 1) * k)); l <= Math.min(j - 1, minL); l++) {
+                    // j - l >= i - 1  => l <= j - i + 1
+                    // ( i - 1 ) * k >= j - l l >= (j - (i - 1) * k)
+                    a = (a + ((res[1][l] * res[i-1][j-l]) % MOD)) % MOD;
                 }
                 res[i][j] = a;
             }
