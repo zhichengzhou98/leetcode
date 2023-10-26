830a01a5b205aff83181f67a63ad4cf5fcf4fd5e
zc.zhou <zzc@renren.com>
2023-10-26 14:34:54 +0800

    BeautifulString

--- src/main/java/com/zzc/leetcode_oct/BeautifulString.java
+++ src/main/java/com/zzc/leetcode_oct/BeautifulString.java
@@ -10,6 +10,7 @@ public class BeautifulString {
         BeautifulString bS = new BeautifulString();
         //954957892
         System.out.println(bS.beautifulString("0100111000100001111101111011010011110101001101110111001101110000110001110101110000000000010101111110"));
+        System.out.println(bS.beautifulString("11"));
     }
     public static final int MOD = 998244353;
 
@@ -26,43 +27,22 @@ public class BeautifulString {
         return (int) res;
     }
 
-    public int calBeautifulCnt(String s, int cntOne, char lastChar) {
+    public double calBeautifulCnt(String s, int cntOne, char lastChar) {
         int len = s.length();
         int newLen = 2 * len - 1;
         int blank = newLen - len;
         double res = 0;
-        if (lastChar == '0') {
-            // 0 的个数至少为 len
-            //已有 0 的个数
-            int existZero = len - cntOne;
-            if (existZero >= len) {
-                return (int) (Math.pow(2, blank) % MOD);
-            }else if (existZero + blank < len) {
-                return 0;
-            }
-            //至少需要
-            int needZero = len - existZero;
-            for (int i = needZero; i <= blank; i++) {
-                res = (res + zuHe(blank, i)) % MOD;
-            }
-            return (int) res;
-        }else {
+        if (lastChar == '1') {
             // 1 的个数至少为 len
-            if (cntOne >= len) {
-                return (int) (Math.pow(2, blank) % MOD);
-            }else if (cntOne + blank < len) {
-                return 0;
-            }
-            //至少需要
-            int needOne = len - cntOne;
-            for (int i = needOne; i <= blank; i++) {
-                res = (res + zuHe(blank, i)) % MOD;
-            }
-            return (int) res;
+            cntOne = len - cntOne;
         }
+        for (int i = cntOne; i <= blank; i++) {
+            res = (res + zuHe(blank, i)) % MOD;
+        }
+        return res;
     }
 
-    public int zuHe(int num1, int num2) {
+    public double zuHe(int num1, int num2) {
         int min = Math.min(num2, num1 - num2);
         int temp = min;
         double res = 1.0;
@@ -74,6 +54,6 @@ public class BeautifulString {
         for (int i = 1; i <= temp; i++) {
             res /= i;
         }
-        return (int) (res % MOD);
+        return res % MOD;
     }
 }
