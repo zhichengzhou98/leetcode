27f25ba6ba25b11c162e8ae1a397f70f005acb75
zc.zhou <zzc@renren.com>
2024-05-07 11:24:58 +0800

    data format

--- src/main/java/com/zzc/leetcode_may/DailyTemperatures.java
+++ src/main/java/com/zzc/leetcode_may/DailyTemperatures.java
@@ -11,21 +11,22 @@ import java.util.Stack;
 public class DailyTemperatures {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new DailyTemperatures().dailyTemperatures(new int[]{30,60,90})));
+        System.out.println(
+            Arrays.toString(new DailyTemperatures().dailyTemperatures(new int[]{30, 60, 90})));
     }
 
     public int[] dailyTemperatures(int[] temperatures) {
         //单调栈
         int[] res = new int[temperatures.length];
         Stack<int[]> stack = new Stack<>();
-        for (int i = temperatures.length - 1; i >= 0 ; i--) {
+        for (int i = temperatures.length - 1; i >= 0; i--) {
 
             while (!stack.isEmpty() && stack.peek()[0] <= temperatures[i]) {
                 stack.pop();
             }
             if (stack.isEmpty()) {
                 res[i] = 0;
-            }else{
+            } else {
                 res[i] = stack.peek()[1] - i;
             }
             stack.push(new int[]{temperatures[i], i});
