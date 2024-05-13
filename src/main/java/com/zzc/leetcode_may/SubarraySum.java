2bbe9ae551fc89e24d7f3d8ce5ae9318d73f62b0
zc.zhou <zzc@renren.com>
2024-05-13 16:35:22 +0800

    feat: SubarraySum

--- src/main/java/com/zzc/leetcode_may/SubarraySum.java
+++ src/main/java/com/zzc/leetcode_may/SubarraySum.java
@@ -1,19 +1,50 @@
 package com.zzc.leetcode_may;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author zc.zhou
- * @Description 滑动窗口，不定长滑窗
+ * @Description 滑动窗口，不定长滑窗 ×（数组中的元素有可能为负，无法确定左右端点哪个移动）
  * @create 2024-05-13 15:53
  */
 public class SubarraySum {
-    public int subarraySum(int[] nums, int k) {
-        int sum = 0;
-        int left = 0;
-        for (int right = 0; right < nums.length; right++) {
-            sum += nums[right];
-            //TODO
-        }
 
-        return 0;
+    @Test
+    public void test() {
+        System.out.println(subarraySum1(new int[]{1, 1, 1}, 1));
+    }
+
+    public int subarraySum1(int[] nums, int k) {
+        int res = 0;
+        //前缀和
+        int[] prefixSum = new int[nums.length + 1];
+        for (int i = 1; i < prefixSum.length; i++) {
+            prefixSum[i] = prefixSum[i-1] + nums[i-1];
+        }
+        for (int i = 1; i < prefixSum.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (prefixSum[i] - prefixSum[j] == k) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    //前缀和 + hash优化 todo
+    public int subarraySum(int[] nums, int k) {
+        int res = 0;
+        int[] prefixSum = new int[nums.length + 1];
+        for (int i = 1; i < prefixSum.length; i++) {
+            prefixSum[i] = prefixSum[i-1] + nums[i-1];
+        }
+        for (int i = 1; i < prefixSum.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (prefixSum[i] - prefixSum[j] == k) {
+                    res++;
+                }
+            }
+        }
+        return res;
     }
 }
