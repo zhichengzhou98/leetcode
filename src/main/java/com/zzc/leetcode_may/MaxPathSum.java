9648e3aeb2de3d19c4cc6ad0f4063e7a7be97dee
zc.zhou <zzc@renren.com>
2024-05-20 17:16:18 +0800

    feat: maxPathSum

---
+++ src/main/java/com/zzc/leetcode_may/MaxPathSum.java
@@ -0,0 +1,35 @@
+package com.zzc.leetcode_may;
+
+
+/**
+ * @author zc.zhou
+ * @Description
+ * @create 2024-05-20 16:53
+ */
+public class MaxPathSum {
+
+    public int maxPathSum(TreeNode root) {
+        //TODO
+        return 0;
+    }
+}
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
