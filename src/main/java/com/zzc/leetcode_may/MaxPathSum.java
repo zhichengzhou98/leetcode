a399a794555cec0143e980e2f8e9343d1dbd6ce1
zzc <zzc@gmail.com>
2024-05-21 22:11:04 +0800

    05.21 MaxPathSum

--- src/main/java/com/zzc/leetcode_may/MaxPathSum.java
+++ src/main/java/com/zzc/leetcode_may/MaxPathSum.java
@@ -1,6 +1,11 @@
 package com.zzc.leetcode_may;
 
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
 /**
  * @author zc.zhou
  * @Description
@@ -8,9 +13,56 @@ package com.zzc.leetcode_may;
  */
 public class MaxPathSum {
 
+    Map<TreeNode, int[]> map;
+
+    int max;
+
+    @Test
+    public void test() {
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(-1);
+        TreeNode node3 = new TreeNode(-2);
+        node1.left = node2;
+        node1.right = node3;
+        System.out.println(maxPathSum(node1));
+    }
+
     public int maxPathSum(TreeNode root) {
-        //TODO
-        return 0;
+        map = new HashMap<>();
+        max = Integer.MIN_VALUE;
+        dfs(root);
+        return max;
+    }
+
+    public int[] dfs(TreeNode root) {
+        if (map.containsKey(root)) {
+            return map.get(root);
+        }
+        //左（以该节点为终点左边部分） 右 中（经过该节点）
+        int[] res = new int[3];
+        if (root == null) {
+            return res;
+        }
+        res[2] = root.val;
+        if (root.left == null) {
+            res[0] = root.val;
+        } else {
+            int[] resLeft = dfs(root.left);
+            int temp = Math.max(Math.max(resLeft[0], resLeft[1]), 0);
+            res[0] = root.val + temp;
+            res[2] += temp;
+        }
+        if (root.right == null) {
+            res[1] = root.val;
+        } else {
+            int[] resRight = dfs(root.right);
+            int temp = Math.max(Math.max(resRight[0], resRight[1]), 0);
+            res[1] = root.val + temp;
+            res[2] += temp;
+        }
+        max = Math.max(Math.max(Math.max(res[0], res[1]), res[2]), max);
+        map.put(root, res);
+        return res;
     }
 }
 
