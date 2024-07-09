3d6d0c7c8c2aebcdc5014bb848ffcad0b0188452
zc.zhou <zzc@renren.com>
2024-07-09 17:37:04 +0800

    07.09 getChebyshevDistance

--- src/main/java/com/zzc/leetcode_jul/MinimumDistance.java
+++ src/main/java/com/zzc/leetcode_jul/MinimumDistance.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * @author zc.zhou
@@ -19,21 +20,31 @@ public class MinimumDistance {
     int[][] points = ArrayUtils.generate("array", int[][].class);
     System.out.println(minimumDistance(points));
   }
+
   public int minimumDistance(int[][] points) {
+    points = Arrays.stream(points).map(point -> new int[]{point[0] + point[1],
+        point[0] - point[1]}).toArray(int[][]::new);
+    System.out.println(Arrays.deepToString(points));
     Arrays.sort(points, (a, b) -> {
       if (a[0] == b[0]) {
         return a[1] - b[1];
       }
       return a[0] - b[0];
     });
+    System.out.println(Arrays.deepToString(points));
     int[] first = points[0];
     int[] second = points[1];
     int[] third = points[points.length - 2];
     int[] forth = points[points.length - 1];
-    return Math.min(getDistance(first, third), getDistance(second, forth));
+    return Math.min(getChebyshevDistance(first, third), getChebyshevDistance(second, forth));
   }
 
-  private int getDistance(int[] begin, int[] end) {
+  private int getManhattanDistance(int[] begin, int[] end) {
     return Math.abs(begin[0] - end[0]) + Math.abs(begin[1] - end[1]);
   }
+
+  private int getChebyshevDistance(int[] begin, int[] end) {
+    return Math.max(Math.abs(begin[0] - end[0]), Math.abs(begin[1] - end[1]));
+  }
+
 }
