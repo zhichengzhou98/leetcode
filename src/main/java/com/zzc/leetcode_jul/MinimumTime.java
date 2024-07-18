b6549b188b98b8e1fac36ecb9f65044398a2ff99
zc.zhou <zzc@renren.com>
2024-07-18 17:33:25 +0800

    07.18 MinimumTime todo

--- src/main/java/com/zzc/leetcode_jul/MinimumTime.java
+++ src/main/java/com/zzc/leetcode_jul/MinimumTime.java
@@ -10,7 +10,7 @@ import java.util.PriorityQueue;
 
 /**
  * @author zc.zhou
- * @Description
+ * @Description TODO 迪杰斯特拉算法
  * @create 2024-07-18 13:32
  */
 public class MinimumTime {
@@ -18,7 +18,7 @@ public class MinimumTime {
   void testFun() {
     int n = 3;
     int[][] edges = {{0, 1, 2}, {1, 2, 1}, {0, 2, 4}};
-    int[] disappear = {1, 1, 5};
+    int[] disappear = {1, 1, 2};
     System.out.println(Arrays.toString(minimumTime(n, edges, disappear)));
   }
 
@@ -47,13 +47,16 @@ public class MinimumTime {
       int[] arr = pq.poll();
       int currentNode = arr[0];
       int distance = arr[1];
+      //???
       if (answer[currentNode] != distance) {
-        //???
         continue;
       }
       for (int[] nexts : lists[currentNode]) {
         int nextNode = nexts[0];
         int length = nexts[1];
+        // distance + length < disappear[nextNode] 在nextNode节点消失之前能到达
+        // answer[nextNode] == -1 -> 没有遍历过nextNode节点
+        // distance + length < answer[nextNode] -> 遍历过nextNode节点但是有更近的距离
         if (distance + length < disappear[nextNode] && (answer[nextNode] == -1 || distance + length < answer[nextNode])) {
           pq.offer(new int[]{nextNode, distance + length});
           answer[nextNode] = distance + length;
