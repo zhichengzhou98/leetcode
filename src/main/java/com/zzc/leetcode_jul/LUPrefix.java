a9bcd4babe695da7cbb205ebc0f390c715b757ee
zc.zhou <zzc@renren.com>
2024-07-24 16:57:32 +0800

    07.24 LUPrefix 使用普通Set

--- src/main/java/com/zzc/leetcode_jul/LUPrefix.java
+++ src/main/java/com/zzc/leetcode_jul/LUPrefix.java
@@ -1,5 +1,6 @@
 package com.zzc.leetcode_jul;
 
+import java.util.HashSet;
 import java.util.TreeSet;
 
 /**
@@ -12,11 +13,43 @@ import java.util.TreeSet;
  * 至少会调用 longest 一次。
  * @create 2024-07-24 16:23
  */
-class LUPrefix {
+
+//使用普通set
+public class LUPrefix {
+  HashSet<Integer> set;
+  //n 为最长上传前缀
+  int n;
+
+  int res = 1;
+
+  public LUPrefix(int n) {
+    set = new HashSet<>();
+    this.n = n;
+    for (int i = 1; i <= n; i++) {
+      set.add(i);
+    }
+  }
+
+  public void upload(int video) {
+    set.remove(video);
+  }
+
+  public int longest() {
+    if (set.isEmpty()) {
+      return n;
+    }
+    while (!set.contains(res)) {
+      res++;
+    }
+    return res - 1;
+  }
+}
+
+class LUPrefix1 {
   TreeSet<Integer> set;
   int n;
 
-  public LUPrefix(int n) {
+  public LUPrefix1(int n) {
     set = new TreeSet<>();
     this.n = n;
     for (int i = 1; i <= n; i++) {
