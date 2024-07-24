6805ed932003a3e16b1d9fdc1cd584a591ff9fd7
zc.zhou <zzc@renren.com>
2024-07-24 16:47:16 +0800

    07.24 LUPrefix 使用TreeSet

--- src/main/java/com/zzc/leetcode_jul/LUPrefix.java
+++ src/main/java/com/zzc/leetcode_jul/LUPrefix.java
@@ -1,26 +1,37 @@
 package com.zzc.leetcode_jul;
 
+import java.util.TreeSet;
+
 /**
  * @author zc.zhou
- * @Description
- *  1 <= n <= 10⁵
- *  1 <= video <= 10⁵
- *  video 中所有值 互不相同 。
- *  upload 和 longest 总调用 次数至多不超过 2 * 10⁵ 次。
- *  至少会调用 longest 一次。
+ * @Description 2424
+ * 1 <= n <= 10⁵
+ * 1 <= video <= 10⁵
+ * video 中所有值 互不相同 。
+ * upload 和 longest 总调用 次数至多不超过 2 * 10⁵ 次。
+ * 至少会调用 longest 一次。
  * @create 2024-07-24 16:23
  */
 class LUPrefix {
+  TreeSet<Integer> set;
+  int n;
 
   public LUPrefix(int n) {
-
+    set = new TreeSet<>();
+    this.n = n;
+    for (int i = 1; i <= n; i++) {
+      set.add(i);
+    }
   }
 
   public void upload(int video) {
-
+    set.remove(video);
   }
 
   public int longest() {
-    return 0;
+    if (set.isEmpty()) {
+      return n;
+    }
+    return set.first() - 1;
   }
 }
