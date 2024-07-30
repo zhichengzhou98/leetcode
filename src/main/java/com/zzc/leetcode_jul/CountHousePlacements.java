5abb3a024de98a98a0d8bc442f2a92b5dc12a3f5
zc.zhou <zzc@renren.com>
2024-07-30 18:28:01 +0800

    07.30 dp

---
+++ src/main/java/com/zzc/leetcode_jul/CountHousePlacements.java
@@ -0,0 +1,14 @@
+package com.zzc.leetcode_jul;
+
+/**
+ * @author zc.zhou
+ * @Description 2320 DP 一条街道有 n 个地块，左右两侧独立计算方案数，然后乘积即可。
+ * 房屋偷盗 proMax版
+ * @create 2024-07-30 18:27
+ */
+public class CountHousePlacements {
+  public int countHousePlacements(int n) {
+
+    return 0;
+  }
+}
