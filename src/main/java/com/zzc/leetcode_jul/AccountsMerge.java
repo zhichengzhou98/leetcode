019d2cc9a558596a6ea9e8b71e68b71d1f5301ce
zc.zhou <zzc@renren.com>
2024-07-30 18:00:03 +0800

    07.30 721并查集

--- src/main/java/com/zzc/leetcode_jul/AccountsMerge.java
+++ src/main/java/com/zzc/leetcode_jul/AccountsMerge.java
@@ -3,7 +3,9 @@ package com.zzc.leetcode_jul;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -11,7 +13,7 @@ import java.util.TreeSet;
 
 /**
  * @author zc.zhou
- * @Description TODO：并查集
+ * @Description 721并查集
  * @create 2024-07-15 9:51
  */
 public class AccountsMerge {
@@ -26,16 +28,45 @@ public class AccountsMerge {
     System.out.println(accountsMerge(res));
 
   }
+
+  //并查集
+  public List<List<String>> accountsMerge(List<List<String>> accounts) {
+    UnionFindV4 uf = new UnionFindV4(accounts.size());
+    for (int i = 0; i < accounts.size(); i++) {
+      for (int j = i + 1; j < accounts.size(); j++) {
+        List<String> account1 = accounts.get(i);
+        List<String> account2 = accounts.get(j);
+        List<String> emails1 = account1.subList(1, account1.size());
+        List<String> emails2 = account2.subList(1, account2.size());
+        if (!Collections.disjoint(emails2, emails1)) {
+          uf.union(i, j);
+        }
+      }
+    }
+    Map<Integer, List<Integer>> map = uf.map;
+    return map.values().stream().map(value -> {
+      //所有相同的账户索引
+      Set<String> set = new HashSet<>();
+      for (Integer integer : value) {
+        List<String> account = accounts.get(integer);
+        set.addAll(account.subList(1, account.size()));
+      }
+      List<String> list = new ArrayList<>(set);
+      Collections.sort(list);
+      list.add(0, accounts.get(value.get(0)).get(0));
+      return list;
+    }).toList();
+  }
+
   //(a) (b) (a, b) => 同一个账户，按照以下逻辑会有两个账户 (a, b) (b)
   public List<List<String>> accountsMerge1(List<List<String>> accounts) {
     //key：邮箱账号，value：name
     Map<Set<String>, String> map = new HashMap<>();
-    for (int i = 0; i < accounts.size(); i++) {
-      List<String> account = accounts.get(i);
+    for (List<String> account : accounts) {
       List<String> emails = account.subList(1, account.size());
       Set<Set<String>> sets = map.keySet();
       boolean flag = false;
-      for(Set<String> set: sets) {
+      for (Set<String> set : sets) {
         //遍历当前人的每个邮箱账户
         for (int j = 1; j < account.size(); j++) {
           if (set.contains(account.get(j))) {
@@ -62,7 +93,7 @@ public class AccountsMerge {
     }).toList();
   }
 
-  public List<List<String>> accountsMerge(List<List<String>> accounts) {
+  public List<List<String>> accountsMergeV1(List<List<String>> accounts) {
     int lastSize = accounts.size();
     List<List<String>> lists = accountsMerge1(accounts);
     //如果合并前后的size不必 说明不能再继续合并了
@@ -71,5 +102,47 @@ public class AccountsMerge {
       lists = accountsMerge1(lists);
     }
     return lists;
+  }
+}
+
+class UnionFindV4{
+  int[] parent;
+  int[] rank;
+
+  Map<Integer, List<Integer>> map;
+  public UnionFindV4(int n) {
+    parent = new int[n];
+    rank = new int[n];
+    map = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+      parent[i] = i;
+      rank[i] = 1;
+      List<Integer> list = new ArrayList<>();
+      list.add(i);
+      map.put(i, list);
+    }
+  }
+
+  public int find(int x) {
+    if (parent[x] != x) {
+      parent[x] = find(parent[x]);
+    }
+    return parent[x];
+  }
+
+  public void union(int x, int y) {
+    int rootX = find(x);
+    int rootY = find(y);
+    if (rootX == rootY) {
+      return;
+    }
+    parent[rootX] = rootY;
+    List<Integer> list = map.get(rootX);
+    map.get(rootY).addAll(list);
+    map.remove(rootX);
+  }
+
+  public boolean isConnected(int x, int y) {
+    return find(x) == find(y);
   }
 }
