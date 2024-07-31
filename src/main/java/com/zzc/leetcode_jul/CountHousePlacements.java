d99af5cbd3a212a7138d5bfc9b93c12eae33417b
zc.zhou <zzc@renren.com>
2024-07-31 11:12:08 +0800

    07.31 matrixExponentiationBySquaring矩阵快速幂

--- src/main/java/com/zzc/leetcode_jul/CountHousePlacements.java
+++ src/main/java/com/zzc/leetcode_jul/CountHousePlacements.java
@@ -2,6 +2,8 @@ package com.zzc.leetcode_jul;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 /**
  * @author zc.zhou
  * @Description 2320 DP 一条街道有 n 个地块，左右两侧独立计算方案数，然后乘积即可。
@@ -10,8 +12,61 @@ import org.junit.jupiter.api.Test;
 public class CountHousePlacements {
   @Test
   void testFun() {
-    System.out.println(getSingleTotal(2));
+    System.out.println(getSingleTotal(5));
   }
+
+  public int[][] matrixExponentiationBySquaring(int[][] matrix, int n, int MOD) {
+    if (n == 1) {
+      return matrix;
+    }
+    if (n % 2 == 0) {
+      int[][] tempMatrix = matrixExponentiationBySquaring(matrix, n / 2, MOD);
+      return matrixMultiplication(tempMatrix, tempMatrix, MOD);
+    }
+    int[][] tempMatrix = matrixExponentiationBySquaring(matrix, (n - 1) / 2, MOD);
+    tempMatrix = matrixMultiplication(tempMatrix, tempMatrix, MOD);
+    return matrixMultiplication(tempMatrix, matrix, MOD);
+  }
+
+  private int[][] matrixMultiplication(int[][] matrixA, int[][] matrixB, int MOD) {
+    int[][] res = new int[matrixA.length][matrixA[0].length];
+    for (int i = 0; i < res.length; i++) {
+      for (int j = 0; j < res[i].length; j++) {
+        //tempMatrix 第i行 * 第j列
+        int[] lineI = matrixA[i];
+        int[] columJ = new int[res[i].length];
+        for (int k = 0; k < columJ.length; k++) {
+          columJ[k] = matrixB[k][j];
+        }
+        res[i][j] = 0;
+        for (int k = 0; k < lineI.length; k++) {
+          res[i][j] = (int) ((res[i][j] + ((long) lineI[k] * columJ[k]) % MOD) % MOD);
+        }
+      }
+    }
+    return res;
+  }
+
+  private long getSingleTotal(int n) {
+    //0: 表示不放置房子
+    //1: 表示放置房子
+    long[][] dp = new long[n][2];
+    dp[0][0] = 1L;
+    dp[0][1] = 1L;
+    if (n == 1) {
+      return 2;
+    }
+    //使用矩阵快速幂 系数矩阵为 A: {{1, 1}, {1, 0}}
+    int[][] matrix = {{1, 1}, {1, 0}};
+    int[][] res = matrixExponentiationBySquaring(matrix, n - 1, MOD);
+    for (int i = 0; i < res[0].length; i++) {
+      dp[n-1][0] = (dp[n - 1][0] + res[0][i]) % MOD;
+      dp[n-1][1] = (dp[n - 1][1] + res[1][i]) % MOD;
+    }
+
+    return (dp[n-1][0] + dp[n-1][1]) % MOD;
+  }
+
 
   private static final int MOD = (int) (Math.pow(10, 9) + 7);
   public int countHousePlacements(int n) {
@@ -20,7 +75,7 @@ public class CountHousePlacements {
   }
 
   //n块地 房子不相邻 方案数
-  private long getSingleTotal(int n) {
+  private long getSingleTotalV1(int n) {
     //0: 表示不放置房子
     //1: 表示放置房子
     long[][] dp = new long[n][2];
--- src/main/java/com/zzc/template/ExponentiationBySquaring.java
+++ src/main/java/com/zzc/template/ExponentiationBySquaring.java
@@ -2,31 +2,72 @@ package com.zzc.template;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 /**
  * @author zc.zhou
  * @Description 快速幂
  * @create 2024-03-27 22:20
  */
 public class ExponentiationBySquaring {
-    @Test
-    public void test() {
-        System.out.println(exponentiationBySquaring(2, 87));
+  @Test
+  public void test() {
+    System.out.println(exponentiationBySquaring(2, 87));
+  }
+
+  public static int MOD = (int) (Math.pow(10, 9) + 7);
+
+
+  public long exponentiationBySquaring(int a, int b) {
+    //求a的b次幂
+    if (b == 1) {
+      return a % MOD;
     }
 
-    public static int MOD = (int) (Math.pow(10, 9) + 7);
-
-
-    public long exponentiationBySquaring(int a, int b) {
-        //求a的b次幂
-        if (b == 1) {
-            return a % MOD;
-        }
-
-        if (b % 2 == 0) {
-            long x = exponentiationBySquaring(a, b/2) % MOD;
-            return x * x % MOD;
-        }
-        long y = exponentiationBySquaring(a, (b-1)/2) % MOD;
-        return y * y % MOD * a % MOD;
+    if (b % 2 == 0) {
+      long x = exponentiationBySquaring(a, b / 2) % MOD;
+      return x * x % MOD;
     }
+    long y = exponentiationBySquaring(a, (b - 1) / 2) % MOD;
+    return y * y % MOD * a % MOD;
+  }
+
+  @Test
+  void name() {
+    int[][] matrix = {{2,1},{3,1}};
+    System.out.println(Arrays.deepToString(matrixExponentiationBySquaring(matrix, 5, 1000000)));
+  }
+
+  //矩阵快速幂
+  public int[][] matrixExponentiationBySquaring(int[][] matrix, int n, int MOD) {
+    if (n == 1) {
+      return matrix;
+    }
+    if (n % 2 == 0) {
+      int[][] tempMatrix = matrixExponentiationBySquaring(matrix, n / 2, MOD);
+      return matrixMultiplication(tempMatrix, tempMatrix, MOD);
+    }
+    int[][] tempMatrix = matrixExponentiationBySquaring(matrix, (n - 1) / 2, MOD);
+    tempMatrix = matrixMultiplication(tempMatrix, tempMatrix, MOD);
+    return matrixMultiplication(tempMatrix, matrix, MOD);
+  }
+
+  private int[][] matrixMultiplication(int[][] matrixA, int[][] matrixB, int MOD) {
+    int[][] res = new int[matrixA.length][matrixA[0].length];
+    for (int i = 0; i < res.length; i++) {
+      for (int j = 0; j < res[i].length; j++) {
+        //tempMatrix 第i行 * 第j列
+        int[] lineI = matrixA[i];
+        int[] columJ = new int[res[i].length];
+        for (int k = 0; k < columJ.length; k++) {
+          columJ[k] = matrixB[k][j];
+        }
+        res[i][j] = 0;
+        for (int k = 0; k < lineI.length; k++) {
+          res[i][j] = (int) ((res[i][j] + ((long) lineI[k] * columJ[k]) % MOD) % MOD);
+        }
+      }
+    }
+    return res;
+  }
 }
