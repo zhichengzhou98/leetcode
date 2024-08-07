942bf9c4909192640fa3bb963d5dae46aa95c737
zc.zhou <zzc@renren.com>
2024-08-07 18:44:32 +0800

    08.07 TreeNodeUtils

--- src/main/java/com/zzc/utils/TreeNodeUtils.java
+++ src/main/java/com/zzc/utils/TreeNodeUtils.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
@@ -14,15 +15,23 @@ import java.util.Stack;
  */
 public class TreeNodeUtils {
   public static void main(String[] args) throws IOException {
-    List<Integer> arr = List.of(1, null, 2, 3);
+    List<Integer> arr = new ArrayList<>();
+    arr.add(1);
+    arr.add(null);
+    arr.add(2);
+    arr.add(3);
+    System.out.println(arr.size());
     Stack<Integer> stack = new Stack<>();
-    for (int i = arr.size() - 1; i >=  0; i++) {
+    for (int i = arr.size() - 1; i >= 0; i--) {
       stack.push(arr.get(i));
     }
     TreeNode treeNode = generate(stack, TreeNode.class, Integer.class);
     System.out.println(treeNode);
   }
-  private TreeNodeUtils(){}
+
+  private TreeNodeUtils() {
+  }
+
   //[1, null, 2, 3]
   public static <T> T generate(Stack<Integer> array, Class<T> treeClass, Class<?> elementClass) throws IOException {
     try {
@@ -31,14 +40,18 @@ public class TreeNodeUtils {
       constructor.setAccessible(true); // Make the constructor accessible
       return buildTree(array, 0, constructor, treeClass, elementClass);
     } catch (NoSuchMethodException e) {
-      throw new IOException("TreeNode class must have a constructor with a single value parameter.", e);
+      throw new IOException("TreeNode class must have a constructor with a single value parameter"
+          + ".", e);
     } catch (NoSuchFieldException e) {
       throw new RuntimeException(e);
     }
   }
 
-  private static <T> T generateSingleNode(int value, Constructor<T> constructor,
+  private static <T> T generateSingleNode(Integer value, Constructor<T> constructor,
                                           Class<T> treeClass, Class<?> elementClass) throws NoSuchFieldException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    if (value == null) {
+      return null;
+    }
     // 获取名为 "val" 的字段
     Field val = treeClass.getDeclaredField("val");
     // 使字段可访问，即使它是私有的
@@ -53,7 +66,8 @@ public class TreeNodeUtils {
   }
 
   private static <T> T buildTree(Stack<Integer> array, int index, Constructor<T> constructor,
-                                 Class<T> treeClass, Class<?> elementClass) throws IOException, NoSuchFieldException {
+                                 Class<T> treeClass, Class<?> elementClass) throws IOException,
+      NoSuchFieldException {
     if (index >= array.size() || array.get(index) == null) {
       return null;
     }
@@ -68,42 +82,44 @@ public class TreeNodeUtils {
 
 
     //已经创建好node的集合 某一层的树的节点
-    Stack<T> nodeStack = new Stack<>();
+    Stack<Integer> nodeStack = new Stack<>();
+    nodeStack.push(array.pop());
     try {
-      // 创建根节点
-      T node = constructor.newInstance();
-      if (elementClass == Integer.class || elementClass == int.class) {
-        val.setInt(node, array.get(index)); // 设定 val 的值为 array[index]
-      } else if (elementClass == String.class) {
-        val.set(node, array.get(index)); // 设定 val 的值为 array[index]
-      }
-      //将根节点加入
-      nodeStack.push(node);
-      //根节点为第0层，从第1层开始
-      //表示下一次将从array中取出2个元素
-      int layer = 1;
-      while (!array.isEmpty()) {
-        //
-        while (!nodeStack.isEmpty() ) {
-          //栈顶元素弹出
-          T pop = nodeStack.pop();
-          //left.set(pop, );
-          /*if (pop === null || pop.val === null) {
-            continue;
-          }*/
+
+      T root = null;
+      while (!nodeStack.isEmpty()) {
+        //栈顶元素弹出
+        Integer pop = nodeStack.pop();
+        if (pop != null) {
+          T currentNode = generateSingleNode(pop, constructor, treeClass, elementClass);
+          if (root == null) {
+            root = currentNode;
+          }
+          //构建左右节点
+          if (!array.isEmpty()) {
+            Integer leftValue = array.pop();
+            //左节点加入 nodeStack
+            nodeStack.push(leftValue);
+            T leftNode = generateSingleNode(leftValue, constructor, treeClass, elementClass);
+            left.set(currentNode, leftNode);
+          } else {
+            break;
+          }
+          if (!array.isEmpty()) {
+            Integer rightValue = array.pop();
+            //左节点加入 nodeStack
+            nodeStack.push(rightValue);
+            T rightNode = generateSingleNode(rightValue, constructor, treeClass, elementClass);
+            right.set(currentNode, rightNode);
+          } else {
+            break;
+          }
         }
       }
 
-      // Get left and right child nodes
-      T leftChild = buildTree(array, 2 * index + 1, constructor, treeClass, elementClass);
-      T rightChild = buildTree(array, 2 * index + 2, constructor, treeClass, elementClass);
-
-      // Set left and right child nodes using reflection
-      left.set(node, leftChild);
-      right.set(node, rightChild);
-
-      return node;
-    } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchFieldException e) {
+      return root;
+    } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+             NoSuchFieldException e) {
       throw new IOException("Failed to create tree node.", e);
     }
   }
---
+++ src/main/java/com/zzc/utils/Valuable.java
@@ -0,0 +1,10 @@
+package com.zzc.utils;
+
+/**
+ * @author zc.zhou
+ * @Description
+ * @create 2024-08-07 17:42
+ */
+public interface Valuable {
+  int getVal();
+}
