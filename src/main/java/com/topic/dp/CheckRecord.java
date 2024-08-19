d0e564454d68c41acec29ad4f0ef075a788cd8d5
zc.zhou <zzc@renren.com>
2024-08-19 18:42:45 +0800

    08.19 CheckRecord

---
+++ src/main/java/com/topic/dp/CheckRecord.java
@@ -0,0 +1,17 @@
+package com.topic.dp;
+
+/**
+ * @author zc.zhou
+ * @Description 552学生出勤记录2
+ * @create 2024-08-19 16:35
+ */
+
+public class CheckRecord {
+  private static final int MOD = (int) Math.pow(10, 9) + 7;
+  public int checkRecord(int n) {
+    //A L P
+    //dp[i][j][k]: i 字符串的长度 j: 字符A的个数 取值0，1 k: 末尾连续L的个数 0，1，2
+    //初始化
+    return 0;
+  }
+}
