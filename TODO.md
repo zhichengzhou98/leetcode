86c739baa581e12d1ff0dce6422367a65bcde4bc
zc.zhou <zzc@renren.com>
2024-08-23 15:54:51 +0800

    08.23 reverseKGroup

--- TODO.md
+++ TODO.md
@@ -1,5 +1,5 @@
 * TreeNodeUtils 根据树节点数组生成树 (需要测试)
 * 线段树 
 * dijkstra算法 使用优先队列（√ mlogm m为边的数量）  枚举点
-* dp 背包问题
+* dp 背包问题 数位dp
 * 字典树
---
+++ src/main/java/com/topic/dfs/ListNode.java
@@ -0,0 +1,73 @@
+package com.topic.dfs;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author zc.zhou
+ * @Description 25 K个一组翻转链表
+ * @create 2024-08-23 15:25
+ */
+public class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Solution {
+  @Test
+  void testFnu() {
+    ListNode l1 = new ListNode(1);
+    ListNode l2 = new ListNode(2);
+    ListNode l3 = new ListNode(3);
+    ListNode l4 = new ListNode(4);
+    l1.next = l2;
+    l2.next = l3;
+    l3.next = l4;
+    ListNode listNode = reverseKGroup(l1, 3);
+    System.out.println(listNode);
+  }
+  public ListNode reverseKGroup(ListNode head, int k) {
+    int len = 0;
+    //统计链表的长度
+    ListNode current = head;
+    while (current != null) {
+      current = current.next;
+      len++;
+    }
+    return reverse(head, k, len);
+  }
+  //反转链表
+  //len为当前链表的长度
+  private ListNode reverse(ListNode head, int k, int len) {
+    if (len < k) {
+      return head;
+    }
+    // 1 -> 2 -> 3 -> 4
+    //head为反转后的尾节点
+    //反转链表的前k个节点
+    ListNode res = head;//res表示反转后的头节点
+    ListNode current = head.next;
+    int cnt = 1;
+    while (cnt < k) {
+      //取下一个节点
+      ListNode next = current.next;
+      current.next = res;
+      res = current;
+      current = next;
+      cnt++;
+    }
+    head.next = reverse(current, k, len - k);
+    return res;
+  }
+}
