e7c2b29ae3180ed167f93d7525d448d254dd25a7
zc.zhou <zzc@renren.com>
2024-09-09 11:18:05 +0800

    09.09 FindMedianSortedArrays

--- src/main/java/com/topic/binarysearch/FindMedianSortedArrays.java
+++ src/main/java/com/topic/binarysearch/FindMedianSortedArrays.java
@@ -24,7 +24,7 @@ public class FindMedianSortedArrays {
   }
 
   // o(m + n)
-  public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+  public double findMedianSortedArraysV2(int[] nums1, int[] nums2) {
     int m = nums1.length;
     int n = nums2.length;
     int[] merge = new int[m + n];
@@ -57,5 +57,44 @@ public class FindMedianSortedArrays {
       return ((double) merge[mid - 1] + merge[mid]) / 2;
     }
     return merge[mid];
+  }
+
+  // o(log(m + n))
+  public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    // 确保 nums1 是较短的数组
+    if (nums1.length > nums2.length) {
+      return findMedianSortedArrays(nums2, nums1);
+    }
+
+    int m = nums1.length;
+    int n = nums2.length;
+    int totalLeft = (m + n + 1) / 2;
+
+    int left = 0, right = m;
+
+    while (left < right) {
+      int i = left + (right - left) / 2;
+      int j = totalLeft - i;
+
+      if (nums1[i] < nums2[j - 1]) {
+        left = i + 1;
+      } else {
+        right = i;
+      }
+    }
+
+    int i = left;
+    int j = totalLeft - i;
+
+    int nums1LeftMax = (i == 0) ? Integer.MIN_VALUE : nums1[i - 1];
+    int nums1RightMin = (i == m) ? Integer.MAX_VALUE : nums1[i];
+    int nums2LeftMax = (j == 0) ? Integer.MIN_VALUE : nums2[j - 1];
+    int nums2RightMin = (j == n) ? Integer.MAX_VALUE : nums2[j];
+
+    if ((m + n) % 2 == 1) {
+      return Math.max(nums1LeftMax, nums2LeftMax);
+    } else {
+      return (Math.max(nums1LeftMax, nums2LeftMax) + Math.min(nums1RightMin, nums2RightMin)) / 2.0;
+    }
   }
 }
