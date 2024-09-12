e72d7d6b564cbece99cf4cc3dfeed5a0582955e1
zc.zhou <zzc@renren.com>
2024-09-12 09:03:40 +0800

    09.12 2576. 双指针

--- src/main/java/com/topic/twopointer/MaxNumOfMarkedIndices.java
+++ src/main/java/com/topic/twopointer/MaxNumOfMarkedIndices.java
@@ -1,4 +1,4 @@
-package com.topic.sort;
+package com.topic.twopointer;
 
 import org.junit.jupiter.api.Test;
 
@@ -6,16 +6,46 @@ import java.util.Arrays;
 
 /**
  * @author zc.zhou
- * @Description 2576. 求出最多标记下标 排序 + 二分 解答错误
+ * @Description 2576. 求出最多标记下标 排序 + 双指针
  * @create 2024-09-12 07:52
  */
 public class MaxNumOfMarkedIndices {
   @Test
   void testFun() {
-    int[] arr = {9,2,5,4};
+    int[] arr = {57,40,57,51,90,51,68,100,24,39,11,85,2,22,67,29,74,82,10,96,14,35,25,76,26,54,29,44,63,49,73,50,95,89,43,62,24,88,88,36,6,16,14,2,42,42,60,25,4,58,23,22,27,26,3,79,64,20,92};
     System.out.println(maxNumOfMarkedIndices(arr));
   }
+  //[2,4,5,9] 双指针
   public int maxNumOfMarkedIndices(int[] nums) {
+    int cnts = 0;
+    int n = nums.length;
+    //数字是否被标记
+    boolean[] flag = new boolean[n];
+    int right = n / 2;
+    int left = 0;
+    Arrays.sort(nums);
+    while (right < n) {
+      int numL = nums[left];
+      while (right < n && nums[right] < 2 * numL) {
+        right++;
+      }
+      if (right < n) {
+        flag[right] = true;
+        flag[left] = true;
+        cnts+=2;
+      } else {
+        break;
+      }
+      while (left < n && flag[left]) {
+        left++;
+      }
+      right++;
+    }
+    return cnts;
+  }
+
+  //解答错误
+  public int maxNumOfMarkedIndicesV1(int[] nums) {
     int cnts = 0;
     int n = nums.length;
     //数字是否被标记
