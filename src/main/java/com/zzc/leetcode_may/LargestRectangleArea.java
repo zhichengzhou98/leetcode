e349ab0cfa0cd0c67c655b505ab4f09a91cadd43
zc.zhou <zzc@renren.com>
2024-09-23 16:27:54 +0800

    09.23 84柱状图中的最大的矩形 TODO

--- src/main/java/com/zzc/leetcode_may/LargestRectangleArea.java
+++ src/main/java/com/zzc/leetcode_may/LargestRectangleArea.java
@@ -2,21 +2,20 @@ package com.zzc.leetcode_may;
 
 /**
  * @author zc.zhou
- * @Description
+ * @Description 84柱状图中的最大的矩形
  * @create 2024-05-22 21:35
  */
 public class LargestRectangleArea {
-
-    //o(n²)
-    public int largestRectangleArea(int[] heights) {
-        int res = 0;
-        for (int i = 0; i < heights.length; i++) {
-            int currentMin = heights[i];
-            for (int j = i; j < heights.length; j++) {
-                currentMin = Math.min(currentMin, heights[j]);
-                res = Math.max(res, currentMin * (j - i + 1));
-            }
-        }
-        return res;
+  //o(n²)
+  public int largestRectangleAreaV1(int[] heights) {
+    int res = 0;
+    for (int i = 0; i < heights.length; i++) {
+      int currentMin = heights[i];
+      for (int j = i; j < heights.length; j++) {
+        currentMin = Math.min(currentMin, heights[j]);
+        res = Math.max(res, currentMin * (j - i + 1));
+      }
     }
+    return res;
+  }
 }
