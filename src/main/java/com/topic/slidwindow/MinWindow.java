711ae60a3126eb210492f2885839a1880f785e8a
zc.zhou <zzc@renren.com>
2024-09-26 10:27:59 +0800

    09.26 minWindow

--- src/main/java/com/topic/slidwindow/MinWindow.java
+++ src/main/java/com/topic/slidwindow/MinWindow.java
@@ -56,7 +56,7 @@ public class MinWindow {
     return res;
   }
 
-  public String minWindow(String s, String t) {
+  public String minWindowV2(String s, String t) {
     //统计t每个字符出现的次数
     Map<Character, Integer> map = new HashMap<>();
     for (int i = 0; i < t.length(); i++) {
@@ -89,6 +89,109 @@ public class MinWindow {
       left++;
     }
     return res;
+  }
+
+  public String minWindowV3(String s, String t) {
+    //统计t每个字符出现的次数
+    Map<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < t.length(); i++) {
+      map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
+    }
+    int len = Integer.MAX_VALUE;
+    String res = "";
+    //滑动窗口
+    int left = 0;
+    //枚举右端点
+    for (int right = 0; right < s.length(); right++) {
+      boolean flag = false;
+      map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) - 1);
+      //当满足条件时 左端点左移 直到不满足条件
+      while (checkCnts(map)) {
+        map.put(s.charAt(left), map.getOrDefault(s.charAt(left), 0) + 1);
+        left++;
+        flag = true;
+      }
+      //不满足条件 left - 1时 是满足条件的
+      if (flag && right - (left - 1) + 1 < len) {
+        //求最小值
+        len = right - (left - 1) + 1;
+        res = s.substring(left - 1, right + 1);
+      }
+    }
+    return res;
+  }
+
+  public String minWindowV4(String s, String t) {
+    //统计t每个字符出现的次数
+    Map<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < t.length(); i++) {
+      map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
+    }
+    int len = Integer.MAX_VALUE;
+    int leftRes = -1;
+    int rightRes = -1;
+    //滑动窗口
+    int left = 0;
+    //枚举右端点
+    for (int right = 0; right < s.length(); right++) {
+      map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) - 1);
+      //当满足条件时 左端点左移 直到不满足条件
+      while (checkCnts(map)) {
+        //满足条件
+        if (right - (left) + 1 < len) {
+          //求最小值
+          len = right - (left) + 1;
+          leftRes = left;
+          rightRes = right + 1;
+        }
+        //左端点左移 直到不满足条件
+        map.put(s.charAt(left), map.getOrDefault(s.charAt(left), 0) + 1);
+        left++;
+
+      }
+    }
+    return leftRes == -1 ? "" : s.substring(leftRes, rightRes);
+  }
+
+  //map => 数组
+  public String minWindow(String s, String t) {
+    //统计t每个字符出现的次数
+    Map<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < t.length(); i++) {
+      map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
+    }
+    int cnts = map.keySet().size();
+    int len = Integer.MAX_VALUE;
+    int leftRes = -1;
+    int rightRes = -1;
+    //滑动窗口
+    int left = 0;
+    //枚举右端点
+    for (int right = 0; right < s.length(); right++) {
+      int curCnt = map.getOrDefault(s.charAt(right), 0) - 1;
+      if (curCnt == 0) {
+        cnts--;
+      }
+      map.put(s.charAt(right), curCnt);
+      //当满足条件时 左端点左移 直到不满足条件
+      while (cnts == 0) {
+        //满足条件
+        if (right - (left) + 1 < len) {
+          //求最小值
+          len = right - (left) + 1;
+          leftRes = left;
+          rightRes = right + 1;
+        }
+        //左端点左移 直到不满足条件
+        curCnt = map.getOrDefault(s.charAt(left), 0) + 1;
+        if (curCnt == 1) {
+          cnts++;
+        }
+        map.put(s.charAt(left), curCnt);
+        left++;
+      }
+    }
+    return leftRes == -1 ? "" : s.substring(leftRes, rightRes);
   }
 
   //判断cnts是否全为0 没有大于0 -> true
