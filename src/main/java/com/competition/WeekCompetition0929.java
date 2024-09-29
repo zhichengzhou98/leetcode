a01eb5bdaedb0511b144a8116a7154955514ea31
zzc <zzc@gmail.com>
2024-09-29 13:22:13 +0800

    09.29 滑动窗口

--- src/main/java/com/competition/WeekCompetition0929.java
+++ src/main/java/com/competition/WeekCompetition0929.java
@@ -2,11 +2,9 @@ package com.competition;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -17,129 +15,61 @@ import java.util.Set;
 public class WeekCompetition0929 {
   @Test
   void testFun() {
-    String s1 = "iqeaouqi";
-    System.out.println(countOfSubstrings(s1, 2));
+    String s1 = "ieaouqqieaouqq";
+    System.out.println(countOfSubstrings(s1, 1));
   }
 
   public long countOfSubstrings(String word, int k) {
+    long l1 = cntK(word, k);
+    long l2 = cntK(word, k + 1);
+    return l1 - l2;
+  }
+
+  /**
+   * 滑动窗口，必须满足一种单调性，right右移满足，left右移不满足
+   * 如果right右移可能满足条件 也可能不满足，则不能使用滑动窗口
+   *
+   * @param word
+   * @param k    区间中的非元音字母的个数
+   * @return
+   */
+  private long cntK(String word, int k) {
     long count = 0L;
-    Deque<Integer> aQ = new ArrayDeque<>();
-    Deque<Integer> eQ = new ArrayDeque<>();
-    Deque<Integer> iQ = new ArrayDeque<>();
-    Deque<Integer> oQ = new ArrayDeque<>();
-    Deque<Integer> uQ = new ArrayDeque<>();
+    Map<Character, Integer> mapIndex = new HashMap<>();
+    mapIndex.put('a', 0);
+    mapIndex.put('e', 1);
+    mapIndex.put('i', 2);
+    mapIndex.put('o', 3);
+    mapIndex.put('u', 4);
     //窗口中非元音字母出现的索引
-    List<Integer> unQ = new ArrayList<>();
+    //0-4：元音字母个数 5:非元音字母个数
+    int[] cnts = new int[6];
     int left = 0;
     //滑动窗口
     //枚举右端点
     for (int right = 0; right < word.length(); right++) {
       char c = word.charAt(right);
-      switch (c) {
-        case 'a':
-          aQ.addLast(right);
-          break;
-        case 'e':
-          eQ.addLast(right);
-          break;
-        case 'i':
-          iQ.addLast(right);
-          break;
-        case 'o':
-          oQ.addLast(right);
-          break;
-        case 'u':
-          uQ.addLast(right);
-          break;
-        default:
-          unQ.add(right);
-      }
+      cnts[mapIndex.getOrDefault(c, 5)]++;
       //当满足条件时 左端点左移 直到不满足条件
-      while (checkCnts(aQ, eQ, iQ, oQ, uQ, unQ, k)) {
+      while (checkCnts(cnts, k)) {
         //满足条件
-        //求出最小left
-        int min1 = Math.min(aQ.peekLast(), eQ.peekLast());
-        int min2 = Math.min(iQ.peekLast(), oQ.peekLast());
-        int min3 = Math.min(min1, min2);
-        int min4 = Math.min(min3, uQ.peekLast());
-        //移除unQ中size - k个元素
-        int index = left;
-        boolean flage = false;
-        if (unQ.size() - 1 - k >= 0) {
-          flage = true;
-          index = unQ.get(unQ.size() - 1 - k);
-        }
-        if (min4 >= index) {
-          //满足条件
-          if (!unQ.isEmpty() && unQ.size() - k >= 0) {
-            min4 = Math.min(min4, unQ.get(unQ.size() - k));
-          }
-          count += (index - min4);
-          handle(aQ, eQ, iQ, oQ, uQ, unQ, index);
-          if (flage) {
-            left = index + 1;
-          } else{
-            left++;
-          }
-        } else {
-          break;
-        }
+        //left右移动
+        cnts[mapIndex.getOrDefault(word.charAt(left), 5)]--;
+        left++;
       }
+      //左端点从[0, left - 1]都满足
+      count += left;
     }
     return count;
   }
 
-  //判断cnts是否全为0 没有大于0 -> true
-  private void handle(Deque<Integer> aQ,
-                      Deque<Integer> eQ,
-                      Deque<Integer> iQ,
-                      Deque<Integer> oQ,
-                      Deque<Integer> uQ,
-                      List<Integer> unQ, int min) {
-    while (!aQ.isEmpty() && aQ.peekFirst() <= min) {
-      aQ.pollFirst();
+  private boolean checkCnts(int[] cnts, int k) {
+    for (int i = 0; i < cnts.length - 1; i++) {
+      if (cnts[i] <= 0) {
+        return false;
+      }
     }
-    while (!eQ.isEmpty() && eQ.peekFirst() <= min) {
-      eQ.pollFirst();
-    }
-    while (!iQ.isEmpty() && iQ.peekFirst() <= min) {
-      iQ.pollFirst();
-    }
-    while (!oQ.isEmpty() && oQ.peekFirst() <= min) {
-      oQ.pollFirst();
-    }
-    while (!uQ.isEmpty() && uQ.peekFirst() <= min) {
-      uQ.pollFirst();
-    }
-    while (!unQ.isEmpty() && unQ.get(0) <= min) {
-      unQ.remove(0);
-    }
-  }
-
-
-  //判断cnts是否全为0 没有大于0 -> true
-  private boolean checkCnts(Deque<Integer> aQ,
-                            Deque<Integer> eQ,
-                            Deque<Integer> iQ,
-                            Deque<Integer> oQ,
-                            Deque<Integer> uQ,
-                            List<Integer> unQ, int k) {
-    if (aQ.size() < 1) {
-      return false;
-    }
-    if (eQ.size() < 1) {
-      return false;
-    }
-    if (iQ.size() < 1) {
-      return false;
-    }
-    if (oQ.size() < 1) {
-      return false;
-    }
-    if (uQ.size() < 1) {
-      return false;
-    }
-    return unQ.size() >= k;
+    return cnts[5] >= k;
   }
 
   public int countOfSubstringsV1(String word, int k) {
