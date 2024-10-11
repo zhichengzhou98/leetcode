0aa050abb6d66a580b799b9a5bbf8c3fb179ae2c
zc.zhou <zzc@renren.com>
2024-10-11 11:17:05 +0800

    10.11 1928. 规定时间内到达终点的最小花费 distances需要记录时间与费用

--- src/main/java/com/topic/dijkstra/MinCost.java
+++ src/main/java/com/topic/dijkstra/MinCost.java
@@ -9,18 +9,21 @@ import java.util.Queue;
 
 /**
  * @author zc.zhou
- * @Description 1928. 规定时间内到达终点的最小花费 TODO: 解答错误
+ * @Description 1928. 规定时间内到达终点的最小花费
  * @create 2024-10-03 16:50
  */
 public class MinCost {
-  int[] distances;
+  int[][] distances;
+
   public int minCost(int maxTime, int[][] edges, int[] passingFees) {
     //节点个数
     int n = passingFees.length;
-    distances = new int[n];
+    distances = new int[n][maxTime + 1];
     //初始化到每个点的距离为最大值
-    Arrays.fill(distances, Integer.MAX_VALUE);
-    distances[0] = passingFees[0];
+    for (int[] dis : distances) {
+      Arrays.fill(dis, Integer.MAX_VALUE);
+    }
+    distances[0][0] = passingFees[0];
     //初始化List
     List[] lists = new List[n];
     for (int i = 0; i < n; i++) {
@@ -34,14 +37,18 @@ public class MinCost {
       lists[x].add(new int[]{y, time});
       lists[y].add(new int[]{x, time});
     }
+    int res = Integer.MAX_VALUE;
     dijkstra(lists, 0, passingFees, maxTime);
-    return distances[n - 1] == Integer.MAX_VALUE ? -1 : distances[n - 1];
+    for (int num : distances[n - 1]) {
+      res = Math.min(res, num);
+    }
+    return res == Integer.MAX_VALUE ? -1 : res;
   }
 
   private void dijkstra(List<int[]>[] lists, int begin, int[] passingFees, int maxTime) {
     //0: 点  1: fee 2: time
     Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-    pq.offer(new int[]{begin, distances[0], 0});
+    pq.offer(new int[]{begin, distances[0][0], 0});
     while (!pq.isEmpty()) {
       int[] current = pq.poll();
       int node = current[0];
@@ -53,12 +60,12 @@ public class MinCost {
         int nextDis = passingFees[nextNode];
         int time = next[1];
         //从当前路径到nextNode的距离 没有之前到nextNode的距离短，跳过更新
-        if (nextDis + fee >= distances[nextNode] || curTime + time > maxTime) {
+        if (curTime + time > maxTime || nextDis + fee >= distances[nextNode][curTime + time]) {
           continue;
         }
         //当前路线到nextNode的距离更近，更新此距离，并把nextNode放入优先队列
-        distances[nextNode] = nextDis + fee;
-        pq.offer(new int[]{nextNode, distances[nextNode], curTime + time});
+        distances[nextNode][curTime + time] = nextDis + fee;
+        pq.offer(new int[]{nextNode, distances[nextNode][curTime + time], curTime + time});
       }
     }
   }
