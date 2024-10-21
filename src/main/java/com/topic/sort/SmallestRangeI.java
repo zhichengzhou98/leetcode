87adee32b876f0b0c807a8d7f961fbbfbc956b8e
zc.zhou <zzc@renren.com>
2024-10-21 17:31:29 +0800

    10.21 910. 最小差值 II

--- src/main/java/com/topic/sort/SmallestRangeI.java
+++ src/main/java/com/topic/sort/SmallestRangeI.java
@@ -1,13 +1,36 @@
 package com.topic.sort;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 
 /**
  * @author zc.zhou
- * @Description 908. 最小差值 I
+ * @Description 908. 最小差值 I / 910. 最小差值 II
  * @create 2024-10-20 08:08
  */
 public class SmallestRangeI {
+  @Test
+  void testFun() {
+    int[] nums = {3,1,10};
+    System.out.println(smallestRangeII(nums, 4));
+  }
+
+  public int smallestRangeII(int[] nums, int k) {
+    int size = nums.length;
+    Arrays.sort(nums);
+    int res = nums[size - 1] - nums[0];
+    for (int i = 0; i < size - 1; i++) {
+      //从小于等于i的索引位置的元素都增大k 大于等于i+1位置的元素的减小k
+      //最大的元素
+      int mx = Math.max(nums[i] + k, nums[size - 1] -k);
+      //最小的元素
+      int mi = Math.min(nums[0] + k, nums[i + 1] -k);
+      res = Math.min(res, mx - mi);
+    }
+    return res;
+  }
+
   public int smallestRangeIV1(int[] nums, int k) {
     if (nums.length == 1) {
       return 0;
@@ -24,7 +47,7 @@ public class SmallestRangeI {
     }
     int max = nums[0];
     int min = nums[0];
-    for(int num : nums) {
+    for (int num : nums) {
       max = Math.max(max, num);
       min = Math.min(min, num);
     }
