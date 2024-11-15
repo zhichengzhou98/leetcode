8f26e5638f861d21af0ac9af1e6b2a885250ee8e
zc.zhou <zzc@renren.com>
2024-11-15 09:34:55 +0800

    11.15 矩阵转置

--- src/main/java/com/topic/baoli/MinFlips.java
+++ src/main/java/com/topic/baoli/MinFlips.java
@@ -1,12 +1,16 @@
 package com.topic.baoli;
 
+import com.zzc.utils.MathUtils;
+
+import static com.zzc.utils.MathUtils.matrixTranspose;
+
 /**
  * @author zc.zhou
  * @Description 3239 最少翻转次数使二进制矩阵回文 I
  * @create 2024-11-15 9:12
  */
 public class MinFlips {
-  public int minFlips(int[][] grid) {
+  public int minFlipsV1(int[][] grid) {
     int res = 0;
     int m = grid.length;
     int n = grid[0].length;
@@ -37,5 +41,27 @@ public class MinFlips {
       }
     }
     return Math.min(res, res1);
+  }
+
+  public int minFlips(int[][] grid) {
+    return Math.min(getMin(grid), getMin(matrixTranspose(grid)));
+  }
+
+  public int getMin(int[][] grid) {
+    int res = 0;
+    int n = grid[0].length;
+    // 按行统计
+    for (int[] ints : grid) {
+      int left = 0;
+      int right = n - 1;
+      while (right > left) {
+        if (ints[left] != ints[right]) {
+          res++;
+        }
+        left++;
+        right--;
+      }
+    }
+    return res;
   }
 }
